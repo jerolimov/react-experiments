bf90e2f27e95c36d48f3efce37266e164b7bf08c
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-06-17 03:16:38 +0200

    Add type-safe setTouched

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Formik, Form, Field, FormikHelpers, FormikErrors } from 'formik';
+import { Formik, Form, Field, FormikHelpers, FormikErrors, FormikTouched } from 'formik';
 
 interface FormValues {
   name: string;
   repo: {
-    url?: string;
+    url: string;
+    type: string;
   };
 }
 
@@ -13,6 +14,7 @@ export default function App() {
     name: '',
     repo: {
       url: '',
+      type: '',
     }
   };
 
@@ -26,7 +28,10 @@ export default function App() {
       errors.name = 'Name is required';
     }
     if (!values.repo.url) {
-      errors.repo.url = 'URL is required';
+      errors.repo.url = 'Repo URL is required';
+    }
+    if (!values.repo.type) {
+      errors.repo.type = 'Repo type is required';
     }
 
     return errors;
@@ -40,44 +45,75 @@ export default function App() {
   return (
     <>
       <Formik initialValues={initialValues} validate={validate} onSubmit={onSubmit}>
-        {(formikProps) => (
-          <Form>
-            <p>
-              <label>
-                Name:<br/>
-                <Field type="text" name="name" />
-                {formikProps.touched.name ? 'Touched ' : ''}
-                {formikProps.touched.name && formikProps.errors.name}
-              </label>
-            </p>
-            <p>
-              <label>
-                URL:<br/>
-                <Field type="text" name="repo.url" />
-                {formikProps.touched.repo?.url ? 'Touched ' : ''}
-                {formikProps.touched.repo?.url && formikProps.errors.repo?.url}
-              </label>
-            </p>
-            <p>
-              <button type="submit">Submit</button>
-            </p>
-            <p>
-              Values: {JSON.stringify(formikProps.values)}
-            </p>
-            <p>
-              Errors: {JSON.stringify(formikProps.errors)}
-            </p>
-            <p>
-              Touched: {JSON.stringify(formikProps.touched)}
-            </p>
-            <p>
-              isSubmitting: {JSON.stringify(formikProps.isSubmitting)}<br/>
-              isValidating: {JSON.stringify(formikProps.isValidating)}<br/>
-              status: {JSON.stringify(formikProps.status)}<br/>
-              submitCount: {JSON.stringify(formikProps.submitCount)}
-            </p>
-          </Form>
-        )}
+        {(formikProps) => {
+
+          const resetForm = () => {
+            formikProps.resetForm();
+          }
+
+          const setTouched = (touched: FormikTouched<FormValues>, shouldValidate?: boolean) => {
+            const prevTouched = formikProps.touched;
+            const mergedTouched: FormikTouched<FormValues> = {
+              name: touched.name ?? prevTouched.name,
+              repo: { ...prevTouched.repo, ...touched.repo },
+            };
+            formikProps.setTouched(mergedTouched, shouldValidate);
+          }
+
+          return (
+            <Form>
+              <p>
+                <label>
+                  Name:<br/>
+                  <Field type="text" name="name" />
+                  <button type="button" onClick={() => setTouched({ name: true }, false)}>Touched</button>
+                  <button type="button" onClick={() => setTouched({ name: false }, false)}>Untouched</button>
+                  {formikProps.touched.name ? 'Touched ' : ''}
+                  {formikProps.touched.name && formikProps.errors.name}
+                </label>
+              </p>
+              <p>
+                <label>
+                  URL:<br/>
+                  <Field type="text" name="repo.url" />
+                  <button type="button" onClick={() => setTouched({ repo: { url: true } }, false)}>Touched</button>
+                  <button type="button" onClick={() => setTouched({ repo: { url: false } }, false)}>Untouched</button>
+                  {formikProps.touched.repo?.url ? 'Touched ' : ''}
+                  {formikProps.touched.repo?.url && formikProps.errors.repo?.url}
+                </label>
+              </p>
+              <p>
+                <label>
+                  Type:<br/>
+                  <Field type="text" name="repo.type" />
+                  <button type="button" onClick={() => setTouched({ repo: { type: true } }, false)}>Touched</button>
+                  <button type="button" onClick={() => setTouched({ repo: { type: false } }, false)}>Untouched</button>
+                  {formikProps.touched.repo?.type ? 'Touched ' : ''}
+                  {formikProps.touched.repo?.type && formikProps.errors.repo?.type}
+                </label>
+              </p>
+              <p>
+                <button type="button" onClick={resetForm}>Reset form</button>
+                <button type="submit">Submit</button>
+              </p>
+              <p>
+                Values: {JSON.stringify(formikProps.values)}
+              </p>
+              <p>
+                Errors: {JSON.stringify(formikProps.errors)}
+              </p>
+              <p>
+                Touched: {JSON.stringify(formikProps.touched)}
+              </p>
+              <p>
+                isSubmitting: {JSON.stringify(formikProps.isSubmitting)}<br/>
+                isValidating: {JSON.stringify(formikProps.isValidating)}<br/>
+                status: {JSON.stringify(formikProps.status)}<br/>
+                submitCount: {JSON.stringify(formikProps.submitCount)}
+              </p>
+            </Form>
+          );
+        }}
       </Formik>
     </>
   );
