bb9a8a5b948566e09b5e99376c046195b65fbbc9
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-10-21 19:50:40 +0200

    Add basic mobx example (without deprecated useObservable)

--- mobx/src/App.tsx
+++ mobx/src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+import { observable, action } from 'mobx';
+import { Observer, observer } from 'mobx-react';
+
+interface Person {
+  name: string;
+}
+
+const p1 = observable<Person>({ name: 'John' });
+const p2 = observable<Person>({ name: 'John' });
+
+const changeName = action((p: Person, name: string) => p.name = name);
+
+const P1 = observer(function P1({ person }: { person: Person }) {
+  console.log('P1 render');
+  return <h1>{person.name}</h1>
+})
+
+const P2 = ({ person }: { person: Person }) => (
+  <Observer>
+    {
+      () => {
+        console.log('P2 render');
+        return <h1>{person.name}</h1>;
+      }
+    }
+  </Observer>
+);
+
+export default function App() {
+  console.log('App render');
+  useEffect(() => {
+    setTimeout(() => changeName(p1, 'Jane'), 1000);
+    setTimeout(() => changeName(p2, 'Jane'), 2000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <P1 person={p1} />
+      <P2 person={p2} />
     </div>
   );
 }
-
-export default App;
