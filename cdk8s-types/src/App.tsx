bc98fe27328e806c5ba2447408c8d0c21cc23d17
Christoph Jerolimov <jerolimov+git+2021@gmail.com>
2023-10-24 14:34:49 +0200

    Use cdk8s with knative types

--- cdk8s-types/src/App.tsx
+++ cdk8s-types/src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { App as K8sApp } from 'cdk8s';
+import { Service, ServiceProps } from '../imports/serving.knative.dev';
+
+interface List<ItemType> {
+  metadata: never;
+  items: ItemType[];
+}
+
+interface ServiceList extends List<ServiceProps> {
+  apiVersion: 'serving.knative.dev/v1';
+  kind: 'ServiceList';
+}
+
+const resourceGroup = `serving.knative.dev/v1`;
+const resourcesName = `services`;
+const namespace = `christoph`;
+
+// It bypass the CORS issue by using the proxy feature of react-scripts.
+// https://create-react-app.dev/docs/proxying-api-requests-in-development/
+// See package.json for the proxt target.
+const url = `/api/kubernetes/apis/${resourceGroup}/namespaces/${namespace}/${resourcesName}`;
+
+const locale = 'de-DE';
+const dateTimeFormat = new Intl.DateTimeFormat(locale, { dateStyle: 'short', timeStyle: 'short' });
+
+// Yep, state.state is annoying, but its just for testing this quickly...
+type State =
+  { state: 'loading', loading: true } |
+  { state: 'loaded', items: ServiceProps[] } |
+  { state: 'failed', error: string };
 
 function App() {
+  const [state, setState] = useState<State>({ state: 'loading', loading: true });
+
+  useEffect(() => {
+    fetch(url).then((response) => {
+      console.log('response', response)
+      if (!response.ok) {
+        throw new Error(`Unxpected status: ${response.status} ${response.statusText}`)
+      }
+      return response.json();
+    }).then((serviceList: ServiceList) => {
+      console.log('jsonResponseBody', serviceList);
+
+      // const app = new K8sApp();
+      // // Test validate function
+      // serviceList.items.forEach((serviceProps) => {
+      //   new Service(app, serviceProps.metadata?.uid, serviceProps);
+      // })
+
+      setState({ state: 'loaded', items: serviceList.items })
+
+    }).catch((error) => {
+      console.error('fetch error:', error);
+      setState({ state: 'failed', error: error.toString() });
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h2>state:</h2>
+      <code>{JSON.stringify(state, null, 2)}</code> */}
+
+      {state.state === 'loading' && <h2>Loading...</h2>}
+      {state.state === 'failed' && <h2>Error: {state.error}</h2>}
+
+      {state.state === 'loaded' && (
+        <>
+          <h2>Resources</h2>
+          <table>
+            <thead>
+              <tr>
+                <th style={{ textAlign: 'left' }}>Type</th>
+                <th style={{ textAlign: 'left' }}>Name</th>
+                <th style={{ textAlign: 'left' }}>URL</th>
+                <th style={{ textAlign: 'left' }}>Created</th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.items.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.metadata?.name}</td>
+                  <td>{item.metadata?.labels?.['function.knative.dev'] === 'true' ? 'Function' : 'Service'}</td>
+                  {/* <td>{item.status?.url && <a href={item.status?.url} target="_blank" rel="noreferrer">{item.status?.url}</a>}</td> */}
+                  <td>{item.metadata?.creationTimestamp && dateTimeFormat.format(new Date(item.metadata.creationTimestamp))}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
+
     </div>
   );
 }
